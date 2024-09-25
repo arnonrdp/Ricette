8443e6cb26ec7f9045747fb33d222df1d7b362d4
Arnon R <arnonrdp@gmail.com>
2024-09-25 14:46:53 -0300

    Refactor SettingsView.vue to handle clearing and restoring recipes

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,19 +1,28 @@
 <template>
   <q-page class="text-center">
-    <q-btn class="q-my-md" color="primary" label="Clear Recipes" @click="clearRecipes" />
+    <q-btn v-if="recipeStore.recipes.length" class="block q-mx-auto q-my-md" color="primary" label="Clear Recipes" @click="clearRecipes" />
+    <q-btn v-else class="block q-mx-auto q-my-md" color="primary" label="Restore Standard Recipes" @click="restoreRecipes" />
   </q-page>
 </template>
 
 <script setup lang="ts">
+import recipes from '@/data/recipes.json'
+import type { Recipe } from '@/model/Recipe'
 import { useRecipeStore } from '@/stores/recipes'
-import { useQuasar } from 'quasar'
+import { LocalStorage, useQuasar } from 'quasar'
 
 const $q = useQuasar()
 const recipeStore = useRecipeStore()
 
 function clearRecipes() {
-  localStorage.removeItem('recipes')
+  LocalStorage.removeItem('recipes')
   $q.notify({ color: 'primary', message: 'Recipes cleared' })
   recipeStore.$reset()
+}
+
+function restoreRecipes() {
+  LocalStorage.set('recipes', recipes)
+  $q.notify({ color: 'primary', message: 'Standard recipes have been added' })
+  recipeStore._recipes = LocalStorage.getItem('recipes') as Recipe[]
 }
 </script>
