8be0ca7effb6a4d743577cd55d2f0603b4df5058
Arnon R <arnonrdp@gmail.com>
2024-09-25 10:07:09 -0300

    Refactor HomeView.vue to use computed property for filtered recipes + TransitionGroup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,88 +6,90 @@
     </section>
 
     <q-list class="q-mt-md">
-      <q-expansion-item
-        v-for="recipe in recipeStore.recipes"
-        :caption="`Complexity: ${recipe.complexity}`"
-        :default-opened="recipe.id === '1'"
-        group="recipe"
-        :key="recipe.id"
-        :label="recipe.name"
-        popup
-      >
-        <q-separator />
-        <q-card>
-          <q-card-section>
-            <h6>{{ recipe.name }}</h6>
-            <p class="text-subtitle1">{{ recipe.description }}</p>
+      <TransitionGroup name="list">
+        <q-expansion-item
+          v-for="recipe in filteredRecipes"
+          :caption="`Complexity: ${recipe.complexity}`"
+          :default-opened="recipe.id === '1'"
+          group="recipe"
+          :key="recipe.id"
+          :label="recipe.name"
+          popup
+        >
+          <q-separator />
+          <q-card>
+            <q-card-section>
+              <h6>{{ recipe.name }}</h6>
+              <p class="text-subtitle1">{{ recipe.description }}</p>
 
-            <q-banner dark>
-              <div class="row">
-                <div class="col-3 col-grow text-center">
-                  <p class="q-mb-none q-mt-md text-bold">SERVINGS:</p>
-                  <p class="text-italic">{{ recipe.servings }}</p>
+              <q-banner dark>
+                <div class="row">
+                  <div class="col-3 col-grow text-center">
+                    <p class="q-mb-none q-mt-md text-bold">SERVINGS:</p>
+                    <p class="text-italic">{{ recipe.servings }}</p>
+                  </div>
+                  <div class="col-3 col-grow text-center">
+                    <p class="q-mb-none q-mt-md text-bold">PREP TIME:</p>
+                    <p class="text-italic">{{ recipe.prepTime }} min</p>
+                  </div>
+                  <div class="col-3 col-grow text-center">
+                    <p class="q-mb-none q-mt-md text-bold">COOK TIME:</p>
+                    <p class="text-italic">{{ recipe.cookTime }} min</p>
+                  </div>
+                  <div v-if="recipe.prepTime && recipe.cookTime" class="col-3 col-grow text-center">
+                    <p class="q-mb-none q-mt-md text-bold">TOTAL TIME:</p>
+                    <p class="text-italic">{{ recipe.prepTime + recipe.cookTime }} min</p>
+                  </div>
                 </div>
-                <div class="col-3 col-grow text-center">
-                  <p class="q-mb-none q-mt-md text-bold">PREP TIME:</p>
-                  <p class="text-italic">{{ recipe.prepTime }} min</p>
-                </div>
-                <div class="col-3 col-grow text-center">
-                  <p class="q-mb-none q-mt-md text-bold">COOK TIME:</p>
-                  <p class="text-italic">{{ recipe.cookTime }} min</p>
-                </div>
-                <div v-if="recipe.prepTime && recipe.cookTime" class="col-3 col-grow text-center">
-                  <p class="q-mb-none q-mt-md text-bold">TOTAL TIME:</p>
-                  <p class="text-italic">{{ recipe.prepTime + recipe.cookTime }} min</p>
-                </div>
-              </div>
-            </q-banner>
+              </q-banner>
 
-            <q-splitter disable :model-value="splitterModel">
-              <template v-slot:before>
-                <div class="q-pa-md">
-                  <h5 class="q-mb-md">Ingredients</h5>
-                  <q-list>
-                    <q-item
-                      v-for="(ingredient, index) in recipe.ingredients"
-                      clickable
-                      :key="index"
-                      v-ripple
-                    >
-                      <q-item-section class="text-body1 text-capitalize">
-                        &bullet; {{ ingredient }}
-                      </q-item-section>
-                    </q-item>
-                  </q-list>
-                  <q-img class="full-width q-mt-md" :src="recipe.imageLink" />
-                </div>
-              </template>
+              <q-splitter disable :model-value="splitterModel">
+                <template v-slot:before>
+                  <div class="q-pa-md">
+                    <h5 class="q-mb-md">Ingredients</h5>
+                    <q-list>
+                      <q-item
+                        v-for="(ingredient, index) in recipe.ingredients"
+                        clickable
+                        :key="index"
+                        v-ripple
+                      >
+                        <q-item-section class="text-body1 text-capitalize">
+                          &bullet; {{ ingredient }}
+                        </q-item-section>
+                      </q-item>
+                    </q-list>
+                    <q-img class="full-width q-mt-md" :src="recipe.imageLink" />
+                  </div>
+                </template>
 
-              <template v-slot:after>
-                <div class="q-pa-md">
-                  <h5 class="q-mb-md">Instructions</h5>
-                  <q-list>
-                    <q-item
-                      v-for="(instruction, index) in recipe.instructions"
-                      clickable
-                      :key="index"
-                      v-ripple
-                    >
-                      <q-item-section class="text-body1 text-capitalize">
-                        &bullet; {{ instruction }}
-                      </q-item-section>
-                    </q-item>
-                  </q-list>
-                </div>
-              </template>
-            </q-splitter>
-          </q-card-section>
+                <template v-slot:after>
+                  <div class="q-pa-md">
+                    <h5 class="q-mb-md">Instructions</h5>
+                    <q-list>
+                      <q-item
+                        v-for="(instruction, index) in recipe.instructions"
+                        clickable
+                        :key="index"
+                        v-ripple
+                      >
+                        <q-item-section class="text-body1 text-capitalize">
+                          &bullet; {{ instruction }}
+                        </q-item-section>
+                      </q-item>
+                    </q-list>
+                  </div>
+                </template>
+              </q-splitter>
+            </q-card-section>
 
-          <q-card-actions align="right">
-            <q-btn color="primary" label="Edit" no-caps @click="onEdit(recipe)" />
-            <q-btn color="negative" label="Delete" no-caps @click="onDelete(recipe.id)" />
-          </q-card-actions>
-        </q-card>
-      </q-expansion-item>
+            <q-card-actions align="right">
+              <q-btn color="primary" label="Edit" no-caps @click="onEdit(recipe)" />
+              <q-btn color="negative" label="Delete" no-caps @click="onDelete(recipe.id)" />
+            </q-card-actions>
+          </q-card>
+        </q-expansion-item>
+      </TransitionGroup>
     </q-list>
 
     <q-dialog v-model="isAdding" @hide="resetRecipe">
@@ -211,7 +213,7 @@
 <script setup lang="ts">
 import type { Recipe } from '@/model/Recipe'
 import { useRecipeStore } from '@/stores/recipes'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const recipeStore = useRecipeStore()
 const splitterModel = ref<number>(50)
@@ -231,6 +233,14 @@ const recipe = ref<Recipe>({
   instructions: []
 })
 const search = ref<string>('')
+
+const filteredRecipes = computed(() => {
+  return recipeStore.recipes.filter(
+    (recipe) =>
+      recipe.name.toLowerCase().includes(search.value.toLowerCase()) ||
+      recipe.description.toLowerCase().includes(search.value.toLowerCase())
+  )
+})
 
 function resetRecipe() {
   isEditing.value = false
@@ -268,3 +278,15 @@ function onDelete(id: string) {
   recipeStore.deleteRecipe(id)
 }
 </script>
+
+<style scoped>
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+</style>
