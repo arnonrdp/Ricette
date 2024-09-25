f671031a5997f212564e37c27d2973c9f43c9989
Arnon R <arnonrdp@gmail.com>
2024-09-25 14:36:02 -0300

    Refactor SettingsView.vue to add functionality for clearing recipes

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,7 +1,19 @@
 <template>
-  <main>
-    <h1>Hello World</h1>
-  </main>
+  <q-page class="text-center">
+    <q-btn class="q-my-md" color="primary" label="Clear Recipes" @click="clearRecipes" />
+  </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRecipeStore } from '@/stores/recipes'
+import { useQuasar } from 'quasar'
+
+const $q = useQuasar()
+const recipeStore = useRecipeStore()
+
+function clearRecipes() {
+  localStorage.removeItem('recipes')
+  $q.notify({ color: 'primary', message: 'Recipes cleared' })
+  recipeStore.$reset()
+}
+</script>
