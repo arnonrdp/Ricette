263aa1278b94dbfba6dfc5fa109780de7037ca27
Arnon R <arnonrdp@gmail.com>
2024-09-25 08:59:14 -0300

    Add recipes data file and initialize local storage with standard recipes

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,19 @@
   </q-layout>
 </template>
 
-<script setup>
-import { RouterView } from 'vue-router'
+<script setup lang="ts">
 import TheHeader from '@/components/TheHeader.vue'
+import recipes from '@/data/recipes.json'
+import { LocalStorage, useQuasar } from 'quasar'
+import { onMounted } from 'vue'
+import { RouterView } from 'vue-router'
+
+const $q = useQuasar()
+
+onMounted(() => {
+  if (!LocalStorage.hasItem('recipes')) {
+    $q.notify({ color: 'primary', message: 'Standard recipes have been added' })
+    LocalStorage.set('recipes', recipes)
+  }
+})
 </script>
